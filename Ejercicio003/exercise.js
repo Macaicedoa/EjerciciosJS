bab839afd725712c49552ed51a0041bc70c8f55e
Macaicedoa <117154985+Macaicedoa@users.noreply.github.com>
2023-11-15 09:17:01 +0100

    Ejercicio003 resuelto

---
+++ Ejercicio003/exercise.js
@@ -0,0 +1,42 @@
+// const sum = function (a, b) {
+//   return a + b;
+// }
+
+// const subtract = function (a, b) {
+//   return a - b;
+// }
+
+// const multiply = function (a, b) {
+//   return a * b;
+// }
+
+// const divide = function (a, b) {
+//   return a / b;
+// }
+
+// const log = function (value) {
+//   console.log(value);
+// }
+
+
+const sum = (a,b) =>{
+  return a + b;
+}
+
+const substract = (a, b) =>{
+  return a - b;
+}
+
+const multiply = (a, b) => {
+  return a * b
+}
+
+const divide = (a, b) => {
+  return a / b;
+}
+
+const log = (value) => {
+  return console.log(value);
+}
+
+log(divide(substract(multiply(sum(2,4),sum(5,2)),2),5)) 
