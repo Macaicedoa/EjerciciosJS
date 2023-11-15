001cb25ce933544a5834a350edd1d74f1a6eb8bf
Macaicedoa <117154985+Macaicedoa@users.noreply.github.com>
2023-11-15 12:19:18 +0100

    correccion

--- Ejercicio005/exercise.js
+++ Ejercicio005/exercise.js
@@ -4,7 +4,7 @@ const person1 = {
   age: 25,
 };
 
-const person2 = {...person1};
+const person2 = person1;
 
 person2.firstName = "Simon";
 
@@ -13,3 +13,6 @@ person2.firstName = "Simon";
 
 console.log(person1);
 console.log(person2);
+
+// The property `firstName` of the `person1` also changes because the copy of the object maps to the memory allocation of the firts object
+// It is a superficial copy of the object and does not create a completely new object. Any change in the copied object will change the original values;
