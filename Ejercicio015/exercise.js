ec4378683365e68af27c85288e391c0ef72c7ebd
Macaicedoa <117154985+Macaicedoa@users.noreply.github.com>
2023-11-17 15:08:41 +0100

    Ejercicio resuelto pero con dudas

---
+++ Ejercicio015/exercise.js
@@ -0,0 +1,30 @@
+function sortPeopleByAge(arr) {
+  let sortedArray = Array();
+  let sortedDummy = Array(); 
+  
+  arr.forEach((element,indx) => {
+    sortedDummy.push([element['age'],indx])
+  });
+  console.log(sortedDummy)
+  sortedDummy.sort();
+  sortedDummy.forEach(item=>{
+    sortedArray.push(arr[item[1]])
+  })
+  return sortedArray  
+}
+
+const people = [
+  { name: 'Paul', age: 16 },
+  { name: 'George', age: 17 },
+  { name: 'Lucas', age: 21 },
+  { name: 'Marco', age: 32 },
+  { name: 'Peter', age: 18 },
+  { name: 'Carl', age: 13 },
+  { name: 'Simon', age: 24 },
+  { name: 'Mark', age: 15 },
+  { name: 'Sandra', age: 34 },
+  { name: 'Alice', age: 28 }
+];
+
+const sortingByAge = sortPeopleByAge(people)
+console.log(sortingByAge)
