02b594af494e331f82b6f70f7464dc8f572e1598
Macaicedoa <117154985+Macaicedoa@users.noreply.github.com>
2023-12-02 17:28:36 +0100

    Ejercicio resuelto

--- JS_Asynch/Ejercicio038/exercise.js
+++ JS_Asynch/Ejercicio038/exercise.js
@@ -10,15 +10,13 @@ let firstPromise = new Promise((resolve,reject)=>{
 
 firstPromise
     .then((number)=>{
-        console.log(number);
-        let secondPromise = new Promise((resolve,reject)=>{
+            new Promise((resolve,reject)=>{
             if(number>=0.5){
                 setTimeout(()=>resolve( `{name: "John", age: 24}`),1000);
             }else{
                 setTimeout(()=>reject(`Error! number is less than 0.5...(${number})`),1000);
             }
-        });
-        secondPromise
+        })
         .then((user)=>console.log(user))
         .catch((err)=>console.error(err))
     })
